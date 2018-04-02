23781748613f44d9e91eb0f3900c73408ec45918
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-02 17:33:23 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(arrays)
+  puts " #{arrays[0]} | #{arrays[1]} | #{arrays[3]} "
+  puts "-----------"
+  puts " #{arrays[4]} | #{arrays[5]} | #{arrays[6]} "
+  puts "-----------"
